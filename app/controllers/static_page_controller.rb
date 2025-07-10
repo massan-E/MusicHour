b22692aebf75968ca56743e3ae36f4af157b4fc4
massan-E <masahiro.greed@gmail.com>
2025-07-11 00:27:07 +0900

    見た目修正

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,7 +1,7 @@
 class StaticPageController < ApplicationController
   def top
     @programs = Program.includes(:user).where(publish: true).order(created_at: :desc).limit(5)
-    @letterboxes = Letterbox.includes(:program).where(publish: true).order(created_at: :desc).limit(3)
+    @letterboxes = Letterbox.includes(:program).where(publish: true).order(created_at: :desc).limit(20)
     @rankings = ProgramRanking.all.includes(:program)
   end
 
