60d75b2e0e1e993e6f69f4c3a8744756c3dfcd60
massan-E <masahiro.greed@gmail.com>
2025-07-16 11:59:33 +0900

    各種修正

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -2,7 +2,7 @@ class StaticPageController < ApplicationController
   def top
     @programs = Program.includes(:user).where(publish: true).order(created_at: :desc).limit(5)
     @letterboxes = Letterbox.includes(:program).where(publish: true).order(created_at: :desc).limit(10)
-    @popular_letterbox = Letterbox.includes(:program).left_joins(:letters).group(:id).order("COUNT(letters.id) DESC").limit(15)
+    @popular_letterbox = Letterbox.includes(:program).where(publish: true).left_joins(:letters).group(:id).order("COUNT(letters.id) DESC").limit(15)
     @rankings = ProgramRanking.all.includes(:program)
   end
 
